bfdd8711a263ecf217113f5106710221a44723d7
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-22 02:50:03 -0300

    feat: manifest and icons

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Summer Project Tracker 2023",
+    short_name: "SPT2023",
+    description: "A tracker for summer project 2023",
+    start_url: "/",
+    display: "fullscreen",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
