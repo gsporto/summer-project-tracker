067ac6ea9076cdd4b73d25cdee1c14859980407b
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-24 00:30:26 -0300

    feat: linter config

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 80,
+  useTabs: false,
+};
